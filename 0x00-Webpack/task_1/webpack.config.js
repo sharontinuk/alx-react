c18f0b2574355bbea742b15c380ae8427a9fe055
sharontinuk <112137670+sharontinuk@users.noreply.github.com>
2023-07-24 22:27:09 +0100

    Update webpack.config.js

--- 0x00-Webpack/task_1/webpack.config.js
+++ 0x00-Webpack/task_1/webpack.config.js
@@ -1,1 +1,9 @@
-gfbjghnkgfk.mnfbghj
+const path = require('path');
+
+module.exports = {
+  entry: './js/dashboard_main.js',
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: 'bundle.js',
+  }
+};
