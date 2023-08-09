2e1b2e5dc0dd142756d874a81a591b6b9f6d3689
sharontinuk <112137670+sharontinuk@users.noreply.github.com>
2023-08-09 23:05:36 +0100

    Update index.js

--- 0x04-React_inline_styling/task_1/dashboard/src/index.js
+++ 0x04-React_inline_styling/task_1/dashboard/src/index.js
@@ -1,1 +1,10 @@
-qwerty
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App/App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
