a73f9ae134c0c1fb90960201fa6b89ed84a24eeb
sharontinuk <112137670+sharontinuk@users.noreply.github.com>
2023-08-09 23:44:46 +0100

    Update NotificationItemShape.js

--- 0x04-React_inline_styling/task_1/dashboard/src/Notifications/NotificationItemShape.js
+++ 0x04-React_inline_styling/task_1/dashboard/src/Notifications/NotificationItemShape.js
@@ -1,1 +1,12 @@
-qwert
+import PropTypes from 'prop-types';
+
+const NotificationItemShape = PropTypes.shape({
+  id: PropTypes.number.isRequired,
+  html: PropTypes.shape({
+    __html: PropTypes.string,
+  }),
+  type: PropTypes.string.isRequired,
+  value: PropTypes.string,
+});
+
+export default NotificationItemShape;
