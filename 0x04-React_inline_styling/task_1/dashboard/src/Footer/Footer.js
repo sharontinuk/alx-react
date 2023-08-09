37fbda71f2b00b1f4614a839e5a31fb95edcf69d
sharontinuk <112137670+sharontinuk@users.noreply.github.com>
2023-08-09 23:27:20 +0100

    Update Footer.js

--- 0x04-React_inline_styling/task_1/dashboard/src/Footer/Footer.js
+++ 0x04-React_inline_styling/task_1/dashboard/src/Footer/Footer.js
@@ -1,1 +1,14 @@
-qwert
+import React from 'react';
+import { getFullYear, getFooterCopy } from '../utils/utils';
+
+function Footer() {
+  return (
+    <footer>
+      <p>
+        Copyright {getFullYear()} - {getFooterCopy(true)}
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
