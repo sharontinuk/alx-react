58901424bf65d5e8e63556b9eebfeb7a774a9875
sharontinuk <112137670+sharontinuk@users.noreply.github.com>
2023-08-09 23:32:14 +0100

    Update Login.test.js

--- 0x04-React_inline_styling/task_1/dashboard/src/Login/Login.test.js
+++ 0x04-React_inline_styling/task_1/dashboard/src/Login/Login.test.js
@@ -1,1 +1,18 @@
-qwert
+import React from 'react';
+import { shallow } from 'enzyme';
+import Login from './Login';
+import { StyleSheetTestUtils } from 'aphrodite';
+
+describe('<Login />', () => {
+  beforeAll(() => {
+    StyleSheetTestUtils.suppressStyleInjection();
+  });
+  afterAll(() => {
+    StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
+  });
+
+  it('render without crashing', () => {
+    const wrapper = shallow(<Login />);
+    expect(wrapper.exists());
+  });
+});
