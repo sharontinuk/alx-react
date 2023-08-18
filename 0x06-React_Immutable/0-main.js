b4b2d09ac62eb731d580f0330c1064b9c4548df9
sharontinuk <112137670+sharontinuk@users.noreply.github.com>
2023-08-18 01:50:37 +0100

    Update 0-main.js

--- 0x06-React_Immutable/0-main.js
+++ 0x06-React_Immutable/0-main.js
@@ -1,1 +1,10 @@
-fgvcdvjfdghbdjhg
+import getImmutableObject from './0-fromjs';
+
+const obj = {
+  fear: true,
+  smell: -1033575916.9145899,
+  wall: false,
+  thing: -914767132,
+};
+
+console.log(getImmutableObject(obj));
