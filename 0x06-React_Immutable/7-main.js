4afbcaf6dc400a36f49d9196caadebf4d64e654e
sharontinuk <112137670+sharontinuk@users.noreply.github.com>
2023-08-18 02:01:16 +0100

    Update 7-main.js

--- 0x06-React_Immutable/7-main.js
+++ 0x06-React_Immutable/7-main.js
@@ -1,1 +1,13 @@
-hbgvdsmb,dvs 
+import { Map } from 'immutable';
+import areMapsEqual from './7-equality';
+
+const map1 = new Map({
+  firstName: 'Guillaume',
+  lastName: 'Salva',
+});
+const map2 = new Map({
+  firstName: 'Guillaume',
+  lastName: 'Salva',
+});
+
+console.log(areMapsEqual(map1, map2));
