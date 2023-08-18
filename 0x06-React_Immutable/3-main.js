bae0020e36d2f9733f37de7b33b25c238f7989d4
sharontinuk <112137670+sharontinuk@users.noreply.github.com>
2023-08-18 01:55:08 +0100

    Update 3-main.js

--- 0x06-React_Immutable/3-main.js
+++ 0x06-React_Immutable/3-main.js
@@ -1,1 +1,5 @@
-dhbvjghgdbh,kfn
+import { getListObject, addElementToList } from './3-list';
+
+console.log(getListObject(['one', 'two', 'three']));
+
+console.log(addElementToList(['one', 'two', 'three'], 'four'));
