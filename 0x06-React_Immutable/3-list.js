1e783dae95690499e95ff4200916454a25e1db85
sharontinuk <112137670+sharontinuk@users.noreply.github.com>
2023-08-18 01:54:26 +0100

    Update 3-list.js

--- 0x06-React_Immutable/3-list.js
+++ 0x06-React_Immutable/3-list.js
@@ -1,1 +1,5 @@
-kejnkbdhvfgdhbejnk
+import { List } from 'immutable';
+
+export const getListObject = (array) => List(array);
+
+export const addElementToList = (list, element) => list.push(element);
