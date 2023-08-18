e3fcde064354f3fd9cf10332c3039b628cd2e4e2
sharontinuk <112137670+sharontinuk@users.noreply.github.com>
2023-08-18 01:53:06 +0100

    Update 2-main.js

--- 0x06-React_Immutable/2-main.js
+++ 0x06-React_Immutable/2-main.js
@@ -1,1 +1,13 @@
-ihgcfdxdfnjhk
+import accessImmutableObject from './2-nested';
+
+const obj = accessImmutableObject(
+  {
+    name: {
+      first: 'Guillaume',
+      last: 'Salva',
+    },
+  },
+  ['name', 'first']
+);
+
+console.log(obj);
